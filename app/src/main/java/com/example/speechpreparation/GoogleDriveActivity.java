98e9b938ff2032b43254c8b55b73597b4aff9d0e
Samuel Waters <samrolandwaters@yahoo.com>
2019-02-15 19:13:59 -0800

    improved new speech button

---
+++ app/src/main/java/com/example/speechpreparation/GoogleDriveActivity.java
@@ -0,0 +1,17 @@
+package com.example.speechpreparation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class GoogleDriveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_drive);
+    }
+}
