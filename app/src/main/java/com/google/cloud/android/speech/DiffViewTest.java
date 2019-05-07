c72eddeda8faf4d6d8ece07bc7d636bd1d220b84
Jennifer Nguyen <jlnguy@ucdavis.edu>
2019-05-06 21:37:33 -0700

    Fixed -1 problem.

--- app/src/main/java/com/google/cloud/android/speech/DiffViewTest.java
+++ app/src/main/java/com/google/cloud/android/speech/DiffViewTest.java
@@ -11,6 +11,7 @@ import android.text.Spanned;
 import android.text.style.BackgroundColorSpan;
 import android.text.style.ForegroundColorSpan;
 import android.text.style.UnderlineSpan;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -115,7 +116,7 @@ public class DiffViewTest extends AppCompatActivity implements IScrollListener {
                 "He has forbidden his Governors to pass Laws of immediate and pressing importance, unless suspended in their operation till his Assent should be obtained; and when so suspended, he has utterly neglected to attend to them.\n" +
                 "\n" +
                 "He has refused to pass other Laws for the accommodation of large districts of people, unless those people would relinquish the right of Representation in the Legislature, a right inestimable to them and formidable to tyrants only.";
-        speechToText = "When in the Course of human events it becomes needed for one people to dissolve the political bounds which have connected them with another and to assume among the powers of the earth, the separate and equal station to which the Laws of Nature and of Nature's God entitle us, a decent respect to the opinions of mankind requires that they should declare the causes which impel them to the separation.\n\n   We hold these truths to be self-evident, that all men are created the same, that they are endowed by their Creator with certain unalienable Rights, that among these are Life, Liberty and the pursuit of Happiness. — That to secure these rights, Governments are instituted among Men, deriving their just powers from the consent of the governed, — That whenever any Form of Government becomes destructive of these ends, it is the Right of the People to alter or to abolish it, and to institute new Government, laying its foundation on such principles and organizing its powers in such form, as to them shall seem most likely to effect their Safety and Happiness. Prudence, indeed, will dictate that Governments long established should not be changed for light and transient causes; and accordingly all experience hath shewn that mankind are more disposed to suffer, while evils are sufferable than to right themselves by abolishing the forms to which they are accustomed. But when a long train of abuses and usurpations, pursuing invariably the same Object evinces a design to reduce them under absolute Despotism, it is their right, it is their duty, to throw off such Government, and to provide new Guards for their future security. — Such has been the patient sufferance of these Colonies; and such is now the necessity which constrains them to alter their former Systems of Government. The history of the present King of Great Britain is a history of repeated injuries and usurpations, all having in direct object the establishment of an absolute Tyranny over these States. To prove this, let Facts be submitted to a candid world. He has refused his Assent to Laws, the most wholesome and necessary for the public good. He has forbidden his Governors to pass Laws of immediate and pressing importance, unless suspended in their operation till his Assent should be obtained; and when so suspended, he has utterly neglected to attend to them.";
+        speechToText = "When in the Course of I add words human events it becomes needed fork one people to dissolve the political bounds which have connected them with another and to assume among the powers of the earth, the separate and equal station to which the Laws of Nature and of Nature's God entitle us, a decent respect to the opinions of mankind requires that they should declare the causes which impel them to the separation.\n\n   We hold these truths to be self-evident, that all men are created the same, that they are endowed by their Creator with certain unalienable Rights, that among these are Life, Liberty and the pursuit of Happiness. — That to secure these rights, Governments are instituted among Men, deriving their just powers from the consent of the governed, — That whenever any Form of Government becomes destructive of these ends, it is the Right of the People to alter or to abolish it, and to institute new Government, laying its foundation on such principles and organizing its powers in such form, as to them shall seem most likely to effect their Safety and Happiness. Prudence, indeed, will dictate that Governments long established should not be changed for light and transient causes; and accordingly all experience hath shewn that mankind are more disposed to suffer, while evils are sufferable than to right themselves by abolishing the forms to which they are accustomed. But when a long train of abuses and usurpations, pursuing invariably the same Object evinces a design to reduce them under absolute Despotism, it is their right, it is their duty, to throw off such Government, and to provide new Guards for their future security. — Such has been the patient sufferance of these Colonies; and such is now the necessity which constrains them to alter their former Systems of Government. The history of the present King of Great Britain is a history of repeated injuries and usurpations, all having in direct object the establishment of an absolute Tyranny over these States. To prove this, let Facts be submitted to a candid world. He has refused his Assent to Laws, the most wholesome and necessary for the public good. He has forbidden his Governors to pass Laws of immediate and pressing importance, unless suspended in their operation till his Assent should be obtained; and when so suspended, he has utterly neglected to attend to them.";
 
         SpannableString script = new SpannableString(scriptText);
         SpannableString speech = new SpannableString(speechToText);
@@ -128,16 +129,9 @@ public class DiffViewTest extends AppCompatActivity implements IScrollListener {
         int lastSpace1 =0, lastSpace2 =0, nextSpace1 = 0, nextSpace2=0;
         diff_match_patch.Operation prevOperation = EQUAL;
         LinkedList<diff_match_patch.Diff> me;
-        if(scriptText.replaceAll("[^a-zA-z']", "").length()>speechToText.length()*1.25)
-        {
-            //this assumes that the user always starts the speech from the beginning
-            me = dmp.diff_lineMode(scriptText.replaceAll("[^a-zA-z']", " ").substring(0,(int)(speechToText.replaceAll("[^a-zA-z']", " ").length() * 1.3)).toLowerCase(), speechToText.toLowerCase());
-            script.setSpan(new ForegroundColorSpan(Color.RED), (int)(speechToText.length() * 1.3), scriptText.length(), Spanned.SPAN_INCLUSIVE_INCLUSIVE);
 
-        }
-        else {
-            me = dmp.diff_lineMode(scriptText.replaceAll("[^a-zA-z']", " ").toLowerCase(), speechToText.replaceAll("[^a-zA-z']", " ").toLowerCase());
-        }
+            me = dmp.diff_lineMode(scriptText.concat("END").replaceAll("[^a-zA-z']", " ").toLowerCase(), speechToText.concat("END").replaceAll("[^a-zA-z']", " ").toLowerCase());
+
         // PRINT OUT ALL CONTENT FROM STRING
 
 
@@ -146,8 +140,21 @@ public class DiffViewTest extends AppCompatActivity implements IScrollListener {
             switch(temp.operation)
             {
                 case EQUAL:
-                    if (prevOperation == DELETE || prevOperation == INSERT)
+//                    Log.e("DIFF", "EQUAL - " + temp.text);
+                    if ((prevOperation == DELETE || prevOperation == INSERT) && (scriptStart!=-1 && scriptEnd !=-1) )
                     {
+//                        Log.e("ERRORS", "scriptStart - " + scriptStart + " scriptEnd - " + scriptEnd + " \tspeechStart - " + speechStart + " speechEnd - " + speechEnd );
+
+                        if (scriptStart == -1 || scriptEnd == -1)
+                        {
+                            scriptStart = scriptEnd = currPos1;
+                        }
+                        if (speechStart == -1 || speechEnd == -1)
+                        {
+                            speechStart = speechEnd = currPos2;
+                        }
+//                        Log.e("ERRORS", "script: " + scriptText.substring(scriptStart,scriptEnd) +" \tspeech: "+ speechToText.substring(speechStart,speechEnd));
+
                         Errors singleError = new Errors( scriptStart,  scriptEnd,  speechStart,  speechEnd);
                         errors.add(singleError);
                         scriptStart = scriptEnd = speechStart = speechEnd = -1;
@@ -160,19 +167,25 @@ public class DiffViewTest extends AppCompatActivity implements IScrollListener {
                     prevOperation = EQUAL;
                     break;
                 case INSERT://for 2
-                    templength = temp.text.length();
-                    speechStart = currPos2;
-                    speech.setSpan(new ForegroundColorSpan(Color.RED), currPos2, (speechToText.length()<(currPos2+templength))?(speechToText.length()):(currPos2+=templength), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
-                    speechEnd = currPos2;
-                    prevOperation = INSERT;
-
+                    if (!(temp.text.equals("\\s*")||temp.text.equals("\n"))) {
+//                        Log.e("DIFF", "INSERT - " + temp.text);
+                        templength = temp.text.length();
+                        speechStart = currPos2;
+                        speech.setSpan(new ForegroundColorSpan(Color.RED), currPos2, (speechToText.length() < (currPos2 + templength)) ? (speechToText.length()) : (currPos2 += templength), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+                        speechEnd = currPos2;
+                        prevOperation = INSERT;
+                    }
                     break;
                 case DELETE: //for 1
-                    templength = temp.text.length();
-                    scriptStart = currPos1;
-                    script.setSpan(new ForegroundColorSpan(Color.RED), currPos1, (scriptText.length()<(currPos1+templength))?(scriptText.length()):(currPos1+=templength), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
-                    scriptEnd = currPos1;
-                    prevOperation = DELETE;
+                    if (!(temp.text.equals("\\s*")||temp.text.equals("\\s*\n\\s*"))) {
+//                        Log.e("DIFF", "DELETE - " + temp.text);
+                        templength = temp.text.length();
+                        scriptStart = currPos1;
+                        script.setSpan(new ForegroundColorSpan(Color.RED), currPos1, (scriptText.length() < (currPos1 + templength)) ? (scriptText.length()) : (currPos1 += templength), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+                        scriptEnd = currPos1;
+                        prevOperation = DELETE;
+
+                    }
                     break;
             }
         }
