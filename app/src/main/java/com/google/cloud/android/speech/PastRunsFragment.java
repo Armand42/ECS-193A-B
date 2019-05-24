c1d18cf0da269085c5a2984325ad6cf39dc49bf6
Yara Fawaz <yarafawaz96@gmail.com>
2019-05-24 15:57:02 -0700

    taking out gray overlay from diff view

--- app/src/main/java/com/google/cloud/android/speech/PastRunsFragment.java
+++ app/src/main/java/com/google/cloud/android/speech/PastRunsFragment.java
@@ -33,6 +33,7 @@ import org.json.JSONObject;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
